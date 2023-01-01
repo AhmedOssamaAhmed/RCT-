8c188bf0b15cc68d1f654dd0c91403bc34ffa330
ZiadNasr <ziadnasr988@gmail.com>
2023-01-01 19:12:30 +0200

    RCT-48 RCT-23 Initial View

--- frontend/src/Screens/UserProductsScreen/UserProductsScreen.js
+++ frontend/src/Screens/UserProductsScreen/UserProductsScreen.js
@@ -3,22 +3,44 @@ import TabBar from "../../Components/TabBar/TabBar.js";
 import WhiteContainer from "../../Components/WhiteContainer/WhiteContainer.js";
 import BlueButton from "../../Components/BlueButton/BlueButton.js";
 import "./UserProductsScreen.css";
+import { Link, useNavigate } from "react-router-dom";
+import logo from '../../Assets/rct-logo.png'
 
 function UserProductsScreen(){
-
+    const navigate = useNavigate()
     return(
-        <>
+        <div>
         	<TabBar links={[
         	    ["Events", "/events"],
         	    ["Announcements", "/announcements"],
         	    ["Products", "/products"],
         	]} />
             <div className="products">
-                <WhiteContainer>
+                 <WhiteContainer>
 					<h1>Our Products</h1>
                 </WhiteContainer>
-            </div> 
-        </>
+            </div>
+                <div className="productsList">
+                    <div className="Item">
+                        <h2 className="Title">Test</h2>
+                        <p>Lorum  Lorum Ipsum Lorum Ipsum</p>
+                </div>
+                    <div className="Item">
+                        <h2>Test</h2>
+                        <img src={logo} alt="Invalid" className="ItemPhoto"/>
+                        <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum</p>
+                        <p className="Link" onClick={() => navigate("/Item")}>Item Details</p>
+                </div>
+                    <div className="Item">
+                        <h2>Test</h2>
+                        <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum</p>
+                </div>
+                    <div className="Item">
+                        <h2 className="productTitle">Test</h2>
+                        <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
