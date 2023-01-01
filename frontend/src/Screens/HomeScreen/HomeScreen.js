c935e1d3291034b75abfc17a46fb6bd1bb74c843
ZiadNasr <ziadnasr988@gmail.com>
2023-01-01 23:28:30 +0200

    user Screens

--- frontend/src/Screens/HomeScreen/HomeScreen.js
+++ frontend/src/Screens/HomeScreen/HomeScreen.js
@@ -3,15 +3,19 @@ import React from "react";
 import TabBar from "../../Components/TabBar/TabBar";
 import AdminHomeScreen from "../AdminHomeScreen/AdminHomeScreen";
 import UserHomeScreen from "../UserHomeScreen/UserHomeScreen";
-// import { Link } from "react-router-dom";
+import {get, post} from "../../Hooks/Network.js";
+import { useNavigate } from "react-router-dom";
 
 function HomeScreen() {
+    const navigate=useNavigate()
     const [adminFlag, setAdminFlag] = React.useState(false);
     React.useEffect(() => {
     let token = localStorage.getItem("token");
-    axios.post(`http://localhost/code/rct/users.php`,null,{headers: {"Authorization" : token}} ).then((response) =>{
+    get("is_admin.php", {"Authorization": token}, (response) => {
+        // if not an admin go to login screen
+        setAdminFlag(response.data.isAdmin)
     });
-}, [])
+},[]);
     return (
         <div>
             {adminFlag ? <AdminHomeScreen /> : <UserHomeScreen />}
--- frontend/src/Screens/UserProductsScreen/UserProductsScreen.js
+++ frontend/src/Screens/UserProductsScreen/UserProductsScreen.js
@@ -1,35 +1,27 @@
 import React from "react";
 import TabBar from "../../Components/TabBar/TabBar.js";
-import WhiteContainer from "../../Components/WhiteContainer/WhiteContainer.js";
 import "./UserProductsScreen.css";
 import { useNavigate } from "react-router-dom";
 import logo from '../../Assets/rct-logo.png'
+import ViewingBlock from "../../Components/ViewingBlock/ViewingBlock.js";
+import WhiteContainer from "../../Components/WhiteContainer/WhiteContainer.js";
 import {get} from "../../Hooks/Network.js";
 
 function UserProductsScreen(){
-    // const [products, setProducts] = React.useState([]);
     const navigate = useNavigate();
+
+    // const [products, setProducts] = React.useState([]);
+    
 	const [shownProducts, setShownProducts] = React.useState([]);
     
-	React.useEffect(
+    React.useEffect(
         () => {
-		// get token
-		let token = localStorage.getItem("token");
-	
-		// check to see if token belongs to an adminstrator
-		get("is_admin.php", {"Authorization": token}, (response) => {
-			// if not an admin go to login screen
-			if(response.data.isAdmin === false){
-				navigate("/");
-			}
-		});
+            let token = localStorage.getItem("token");
+            get("products.php", {"Authorization": token}, (response) => {
+                setShownProducts(response.data.products);
 
-		// since user is an admin get products
-		get("products.php", {"Authorization": token}, (response) =>{
-			setShownProducts(response.data.products);
-			console.log(response.data.products);
-		});
-	},[]);
+            });
+        },[]);
 
     return(
         <div>
@@ -38,35 +30,28 @@ function UserProductsScreen(){
         	    ["Announcements", "/announcements"],
         	    ["Products", "/products"],
         	]} />
+            
             <div className="products">
                  <WhiteContainer>
 					<h1>Our Products</h1>
                 </WhiteContainer>
             </div>
-            <div className="productsList">
-                {shownProducts.map((product) =>{
-                    return (
-                    <div className="Item">
-                        <h2 className="Title">{product["name"]}</h2>
-                        <img src={logo} alt="Invalid" className="ItemPhoto"/>
-                        <div>
-                            <p><span className="ProductInfo">Price: </span>{product["price"]}</p>
-                            <p><span className="ProductInfo">Quantities left: </span>{product["quantity"]}</p>
-                            <p className="Link" onClick={() => navigate("/Item")}>Item Details</p>
+        <div className="productsList">
+                        {shownProducts.map((product) =>{
+                            return (
+                            <div className="Item">
+                                <h2 className="Title">{product["name"]}</h2>
+                                <img src={logo} alt="Invalid" className="ItemPhoto"/>
+                                <div>
+                                    <p><span className="ProductInfo">Price: </span>{product["price"]}</p>
+                                    <p><span className="ProductInfo">Quantities left: </span>{product["quantity"]}</p>
+                                    <p className="Link" onClick={() => navigate("")}>Item Details</p>
+                                </div>
                         </div>
-                </div>
-                    );
-                })}
-                    </div>
-                    {/* <div className="Item">
-                        <h2>Test</h2>
-                        <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum</p>
-                </div>
-                    <div className="Item">
-                        <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum</p>
-                </div> */}
+                            );
+                        })}
+                            </div>
             </div>
-        // </div>
     );
 }
 
